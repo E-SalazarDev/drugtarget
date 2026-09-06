7dd392a10ac762b635aa1dda527b10d30e252e63
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-09-05 22:55:19 -0600

    feat: add ProteinService (application layer)

--- app/application/molecule_service.py
+++ app/application/molecule_service.py
@@ -23,4 +23,20 @@ class MoleculeService:
         
         return MoleculeResponse(id= str(saved.id), smiles=saved.smiles, name=saved.name)
     
-    
+
+class ProteinService:
+    def __init__(self, repository: ProteinRepository):
+        self.repository= repository
+        
+        
+    async def create_protein(self, data: ProteinCreate)->ProteinResponse:
+        protein = Protein(sequence= data.sequence, name= data.name, uniprot_id= data.uniprot_id)
+        
+        is_valid, message = protein.validate_structure()
+        if not is_valid:
+            raise ValueError(message)
+        
+        protein_model = ProteinModel(sequence= protein.sequence, name= protein.name, uniprot_id=protein.uniprot_id)
+        saved = await self.repository.add(protein_model)
+        
+        return  ProteinResponse(id= str(saved.id), sequence= saved.sequence, name= saved.name, uniprot_id= saved.uniprot_id )
