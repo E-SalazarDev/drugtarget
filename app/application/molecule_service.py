cccc982e9911d0e6d8ad565b08d67fbaaa853e6a
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-09-10 23:22:03 -0600

    fix: usar smiles validado en molecule_service

--- app/application/molecule_service.py
+++ app/application/molecule_service.py
@@ -1,11 +1,7 @@
 from app.schemas.molecule import MoleculeCreate, MoleculeResponse
-from app.schemas.protein import ProteinCreate, ProteinResponse
 from app.domain.molecule import Molecule
-from app.domain.protein import Protein
 from app.infrastructure.db.models import MoleculeModel
-from app.infrastructure.db.models import ProteinModel
 from app.infrastructure.db.repositories import MoleculeRepository
-from app.infrastructure.db.repositories import ProteinRepository
 
 class MoleculeService:
     def __init__ (self, repository: MoleculeRepository):
@@ -24,19 +20,3 @@ class MoleculeService:
         return MoleculeResponse(id= str(saved.id), smiles=saved.smiles, name=saved.name)
     
 
-class ProteinService:
-    def __init__(self, repository: ProteinRepository):
-        self.repository= repository
-        
-        
-    async def create_protein(self, data: ProteinCreate)->ProteinResponse:
-        protein = Protein(sequence= data.sequence, name= data.name, uniprot_id= data.uniprot_id)
-        
-        is_valid, message = protein.validate_structure()
-        if not is_valid:
-            raise ValueError(message)
-        
-        protein_model = ProteinModel(sequence= protein.sequence.upper(), name= protein.name, uniprot_id=protein.uniprot_id)
-        saved = await self.repository.add(protein_model)
-        
-        return  ProteinResponse(id= str(saved.id), sequence= saved.sequence, name= saved.name, uniprot_id= saved.uniprot_id )
