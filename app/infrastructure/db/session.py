f111914926ee8d0e1412d707d352b37cd8e930eb
J. Eduardo Salazar Tecuapacho <eduardosalazartecuapacho@gmail.com>
2026-09-14 22:08:22 -0600

    fix: cargar variables de entorno en session.py

--- app/infrastructure/db/session.py
+++ app/infrastructure/db/session.py
@@ -1,10 +1,12 @@
-from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 engine = create_async_engine(os.environ["DATABASE_URL"])
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_session() -> AsyncSession:
     async with async_session_maker() as session:
         yield session
